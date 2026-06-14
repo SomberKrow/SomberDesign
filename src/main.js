db63fac1fca17db532c080d3bb3942f3954334ef
Jake <219918318+SomberKrow@users.noreply.github.com>
2026-06-14 15:19:05 -0400

    Install Vue Router with clean section navigation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { installSectionNavigation } from './router/sectionNavigation'
 import './assets/styles/main.scss'
 import './assets/styles/site.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+router.isReady().then(() => {
+  app.mount('#app')
+  installSectionNavigation(router)
+})
