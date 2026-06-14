aa427ff189ce0214b529deb818970c37b827f860
Jake <219918318+SomberKrow@users.noreply.github.com>
2026-06-14 15:26:28 -0400

    Use the rebuilt app navigation directly

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { installSectionNavigation } from './router/sectionNavigation'
 import './assets/styles/main.scss'
 import './assets/styles/site.scss'
 
@@ -11,5 +10,4 @@ app.use(router)
 
 router.isReady().then(() => {
   app.mount('#app')
-  installSectionNavigation(router)
 })
